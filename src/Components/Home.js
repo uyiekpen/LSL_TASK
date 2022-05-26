c478baffb2db0d994043082bb44ac3310cdae729
unknown <uyiekpenelizabeth@gmail.com>
2022-05-26 22:57:38 +0100

    First commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import HomeView from './HomeView'
+import styled from "styled-components"
 
 const Home = () => {
   return (
-    <div>
+    <Container>
         <HomeView/>
-    </div>
+    </Container>
   )
 }
 
 export default Home
+
+const Container = styled.div`
+height: 100vh;
+width:100vw ;
+`
--- src/Components/HomeView.js
+++ src/Components/HomeView.js
@@ -5,13 +5,12 @@ import axios from "axios"
 const HomeView = () => {
     const [data , setData] = React.useState([])
 
-    const api_host = 'movie-database-alternative.p.rapidapi.com';
-    const api_key = "'a29d108543msh05466b581fe504bp1ac8b7jsn7448e32e1a9d"
-    const url = 'https://movie-database-alternative.p.rapidapi.com/';
+    const url = "https://www.fakerestapi.com/datasets/api/v1/movie-details-dataset.json";
     
     const Getdata  = async () => {
-        const res = await axios.get(url, {params : {s: 'Avengers Endgame', r: 'json', page: '1',api_key,api_host}})
-        console.log(res.data)
+        const res = await axios.get(url)
+        console.log(res.data.data)
+        setData(res.data.data)
 
 
     }
@@ -29,10 +28,21 @@ const HomeView = () => {
                 <Button>Search</Button>
                 </InputHolder>
             </SearcBar>
-            <ContentHolder>
-                <Title>All  movies</Title>
+            <Title>All  movies</Title>
 
-                <Card></Card>
+            <ContentHolder>
+
+                {
+                    data?.map((props)=>(
+                        <Card>
+                    <Image src={props.poster}/>
+                   <TextHolder>
+                   <Title2>Title:{props.title}</Title2>
+                   <Produced>directed_by:{props.directed_by}</Produced>
+                   </TextHolder>
+                </Card>
+                    ))
+                }
             </ContentHolder>
 
         </Wrapper>
@@ -41,39 +51,83 @@ const HomeView = () => {
 }
 
 export default HomeView
+
+const Produced = styled.div`
+margin-top:5px ;
+
+`
+const Title2 = styled.div`
+margin-top:5px ;
+
+`
+const Desc = styled.div`
+margin-top:5px ;
+`
+const TextHolder = styled.div`
+padding-left:10px ;
+`
+
+const Image = styled.img`
+height:250px ;
+width: 300px;
+object-fit: cover;
+
+`
+
+
 const Card = styled.div`
 height: 400px;
-width: 400px;
+width: 300px;
 background: white;
 box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-border-raduis: 20px;
+border-radius: 20px;
+text-align:left ;
+margin-top: 20px;
 `
 const ContentHolder = styled.div`
 margin-top: 20px;
 text-align : center;
+display: flex;
+flex-direction: row;
+flex-wrap:wrap;
+justify-content: space-between;
+
 `
 const Button = styled.button`
 height: 50px;
 width: 100px;
-border-raduis: 15px;
+border-radius: 15px;
 border: none;
-background: white;
-color: black;
-
+background: black;
+color: white;
 `
 const Input = styled.input`
 height: 50px;
 width: 400px;
-background: white;
+background: whitesmoke;
 border: none;
-border-raduis: 13px;
+border-radius: 13px;
 outline: none;
+@media screen and (max-width: 768px){
+    height: 50px;
+width: 250px;
+background: whitesmoke;
+border: none;
+border-radius: 13px;
+outline: none;
+}
 `
 const InputHolder = styled.div`
 display: flex;
 height: 50px;
 width: 550px;
 justify-content: space-evenly;
+@media screen and (max-width: 768px){
+display: flex;
+height: 50px;
+width: 300px;
+justify-content: space-evenly;
+}
 `
 const Title = styled.div`
 font-size: 20px;
@@ -87,7 +141,15 @@ display: flex;
 justify-content: center;
 flex-direction: column;
 align-items: center;
+@media screen and (max-width: 768px){
+height : 300px;
+margin-top : 70px;
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
 
+}
 `
 const Wrapper = styled.div`
 height : 100vh;
@@ -101,7 +163,5 @@ height : 100vh;
 width: 100vw;
 display: flex;
 justify-content: center;
-align-itema: center;
-background-color: whitesmoke;
-
+align-items: center;
 `
